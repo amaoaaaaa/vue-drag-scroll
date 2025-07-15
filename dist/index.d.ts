042f501056714d56fed994a7307a599b6134b548
阿茂一米六 <z.d.mao@qq.com>
2025-07-15 11:27:51 +0800

    🐳 chore: 删除 dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { App } from 'vue';
-import { default as dragScroll } from './directive';
-declare const _default: {
-    install(app: App): void;
-};
-export default _default;
-export { dragScroll };
